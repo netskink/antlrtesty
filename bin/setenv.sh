3c6e8c17787938e18f41b4c9dcfd11523622ce7a
John F. Davis <john.davis@broadcom.com>
2023-12-26 13:49:35 -0500

    wip

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,13 +1,35 @@
 
 
+#
+# github codespace
+#
+#ROOTDIR="/workspaces/antlrtesty/"
+
+
+#
+# macos terminal
+#
+ROOTDIR="${HOME}/progs/github/antlrtesty/"
 
 #export CLASSPATH=".:/usr/local/lib/antlr-4.13-1.jar:$CLASSPATH"
-export CLASSPATH=".:/workspaces/antlrtesty/jars/antlr-4.13.1-complete.jar:$CLASSPATH"
+# github codespace
+#export CLASSPATH=".:/workspaces/antlrtesty/jars/antlr-4.13.1-complete.jar:$CLASSPATH"
+# macos terminal
+export CLASSPATH=".:${ROOTDIR}/jars/antlr-4.13.1-complete.jar:$CLASSPATH"
 
-alias antlr4='java -jar /workspaces/antlrtesty/jars/antlr-4.13.1-complete.jar'
 
-alias grun='java org.antlr.v4.runtime.misc.TestRig'
+
+
+# github codespace
+#alias antlr4='java -jar /workspaces/antlrtesty/jars/antlr-4.13.1-complete.jar'
+# macos terminal
+alias antlr4="java -jar ${ROOTDIR}/jars/antlr-4.13.1-complete.jar"
+
+alias grun="java org.antlr.v4.runtime.misc.TestRig"
 
 
 # GN
-export PATH="/workspaces/antlrtesty/gn/out:$PATH"
+# github codespace
+#export PATH="/workspaces/antlrtesty/gn/out:$PATH"
+# macos terminal
+export PATH="${ROOTDIR}/gn/out:$PATH"
