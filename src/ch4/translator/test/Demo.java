4c65adfaa58841f4cc8136bc906b97f1917b21b5
John F. Davis <john.davis@broadcom.com>
2024-04-30 08:54:37 -0700

    wip

---
+++ src/ch4/translator/test/Demo.java
@@ -0,0 +1,8 @@
+import java.util.list;
+import java.util.Map;
+
+public class Demo {
+    void f(int x, String y) {}
+    int[ ] g(/* no args */) { return null; }
+    List<Map<String, Integer>>[] h() { return null; }
+}
---
+++ src/ch4/translator/test/ExtractInterfaceTool.java
@@ -0,0 +1,60 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.io.FileInputStream;
+import java.io.InputStream;import org.antlr.v4.runtime.tree.*;
+
+public class ExtractInterfaceTool {
+
+    public static void main(String[] args) throws Exception {
+        String inputFile = null;
+        if (args.length > 0) {
+            inputFile = args[0];
+        }
+
+        // Geneeric input stream
+        InputStream is = System.in;
+        if (inputFile != null) {
+            is  = new FileInputStream(inputFile);
+        }
+
+        // ANTLR input stream
+        ANTLRInputStream input = new ANTLRInputStream(is);
+
+        //
+        // Above here its boilerplate
+        //
+
+        // Lexer
+        LabeledExprLexer lexer = new LabeledExprLexer(input);
+
+        // Token Stream - this is the same
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Parser 
+        LabeledExprParser parser = new LabeledExprParser(tokens);
+
+        // Start prarsing - this is the same
+        ParseTree tree = parser.prog(); // parse; start at prog
+
+        // This code is different from the tour.  It is not
+        // shown on page 41 and instead is on page 42.  It uses
+        // the other source file in the test dir.
+        EvalVisitor eval = new EvalVisitor();
+        eval.visit(tree);
+
+        // Added for new translator code
+        JavaLexer lexer = new JavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
+        ParseTree tree = parser.compilationUnit(); // parse
+
+        ParseTreeWalker walker = new ParseTreeWalker();  // create standard walker
+        ExtractInterfaceListener extractor = new ExtractInterfaceListener(parser);
+        walker.walk(extractor, tree);
+
+
+    }
+
+    
+}
